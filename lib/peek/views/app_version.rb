4f93f337b03a09cf2c39f6205b5d1221513e48ba
Kamisama <kami@kamisama.me>
2014-07-30 13:09:13 -0400

    Add more code documentation

--- lib/peek/views/app_version.rb
+++ lib/peek/views/app_version.rb
@@ -1,10 +1,16 @@
 module Peek
   module Views
+    # AppVersion view
     class AppVersion < View
+      # Setting up the peek views
+      # Accepted keys in options:
+      # - app_name: Name of your Rails application (default: Rails)
       def initialize(options = {})
         @app_name = options.delete(:app_name) || Rails
       end
 
+      # Return the application version number is set
+      # Else, build it from the git tag
       def app_version
         @app_version ||= (@app_name::VERSION || `git describe --tags`.strip)
       end
